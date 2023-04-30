62cd72d0c40a22fba1af9caef23fcc686bff1d99
SumayaMalalane <smalalane3@gmail.com>
2023-04-30 17:58:04 +0200

    ultima versao da lista com os dois botoes

--- src/components/table.js
+++ src/components/table.js
@@ -3,7 +3,13 @@ import React from "react";
 export default function table(props) {
     const students = props.props
 return (
-    <table className="table">
+
+  <div  className="container">
+
+    <h1>Lista de presenças</h1>
+
+    
+<table className="table">
   <thead>
     <tr>
       <th scope="col">Nr Estudante</th>
@@ -20,11 +26,14 @@ return (
           <th scope="row">{ student.id }</th>
           <td>{ student.name }</td>
           <td>{ student.surname }</td>
-          <td><button className="btn btn-success">Presente</button></td>
+          <td><button className="btn btn-success">Presente</button>  <button className="btn btn-danger">Ausente</button></td>
         </tr>
 
     ))}
   </tbody>
 </table>
+
+  </div>
+ 
 );
 }
