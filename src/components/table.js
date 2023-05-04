5cb7f98bfdcfb78ab36e0aca5d984cab9d9983d1
samuellangajr <samuellangajr@gmail.com>
2023-05-04 10:57:48 +0200

    Events on bottons

--- src/components/table.js
+++ src/components/table.js
@@ -1,47 +1,66 @@
 import React from "react";
 
+export default function Table(props) {
+  const [selectedButtons, setSelectedButtons] = React.useState({});
 
-export default function table(props) {
-  // entregamos o props (forma de acessar os dados de um outro componete) 
-    const students = props.props
-    // a variavel "students"=recebe o props(que leva os dados),logo a variavel students leva esses dados
-    
+  function handleClickButton1(index) {
+    setSelectedButtons(prevState => {
+      return { ...prevState, [index]: 'presente' };
+    });
+  }
 
+  function handleClickButton2(index) {
+    setSelectedButtons(prevState => {
+      return { ...prevState, [index]: 'ausente' };
+    });
+  }
 
-return (
+  const students = props.props;
 
-  <div  className="container">
-
-    <h1 >Lista de presenças</h1>
-    <br></br>
-    <br></br>
-
-    
-<table className="table table-bordered">
-  <thead>
-    <tr>
-      <th scope="col">Nr Estudante</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Apelido</th>
-      <th scope="col">Presente/Ausente</th>
-    </tr>
-
-  </thead>
-
-  <tbody>
-    {students.map((student, index) =>(
-        <tr data-index={index}>
-          <th scope="row">{ student.id }</th>
-          <td>{ student.name }</td>
-          <td>{ student.surname }</td>
-          <td><button className="btn btn-outline-success">Presente</button>  <button className="btn btn-outline-danger">Ausente</button></td>
-        </tr>
-
-    ))}
-  </tbody>
-</table>
-
-  </div>
- 
-);
+  return (
+    <div className="container my-5 text-center">
+      <h1>Lista de presenças</h1>
+      <br />
+      <br />
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">Nr Estudante</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Apelido</th>
+            <th scope="col">Presente/Ausente</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student, index) => (
+            <tr data-index={index} key={index}>
+              <th scope="row">{student.id}</th>
+              <td>{student.name}</td>
+              <td>{student.surname}</td>
+              <td>
+                <button
+                  className={
+                    "btn btn-outline-success " +
+                    (selectedButtons[index] === "presente" ? "active" : "")
+                  }
+                  onClick={() => handleClickButton1(index)}
+                >
+                  Presente
+                </button>{" "}
+                <button
+                  className={
+                    "btn btn-outline-danger " +
+                    (selectedButtons[index] === "ausente" ? "active" : "")
+                  }
+                  onClick={() => handleClickButton2(index)}
+                >
+                  Ausente
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
