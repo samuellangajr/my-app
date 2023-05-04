c18ec3a6798c5b49b64fd84301810617aa040825
SumayaMalalane <smalalane3@gmail.com>
2023-05-04 11:17:33 +0200

    Acrescimo de mais objectos na lista

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import Table from './components/table'
+import Table from './components/table';
 function App() {
 
   var data = 
   [
-    {'id': 2, 'name': 'John', 'surname': 'Josh'},
-    {'id': 3, 'name': 'Selena', 'surname': 'Matapa'},
+    {'id': 2000, 'name': 'John', 'surname': 'Josh', 'state':1},
+    {'id': 3890, 'name': 'Selena', 'surname': 'Matapa'},
+    {'id': 6790, 'name': 'Samuel', 'surname': 'Langa'},
+    {'id': 5542, 'name': 'Hassan', 'surname': 'Mutole'},
+    {'id': 6773, 'name': 'Lecticia', 'surname':'Cuna'},
+    {'id': 4873, 'name': 'Sibiao', 'surname': 'Marilia'},
+    {'id': 6367, 'name': 'Sandra', 'surname': 'Mutavele'},
+    {'id': 7862, 'name': 'Alfredo', 'surname': 'Rosa'},
+    {'id': 5624, 'name': 'Neylik', 'surname': 'Mohamed'},
+    {'id': 9815, 'name': 'Shelsea', 'surname': 'Orlando'},
+    {'id': 1098, 'name': 'Victoria', 'surname': 'Luis'},
+    {'id': 7601, 'name': 'Serena', 'surname': 'Maconne'},
   ]
     return (
       <Table props={data} ></Table>
